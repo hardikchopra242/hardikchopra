57674ea094124e969985ec2f5fa14f45877e55c2
hardikchopra242 <hardikchopra242@gmail.com>
2021-07-09 01:59:47 +0530

    [Mod] changed Background color

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -17,42 +17,45 @@ const About = () => {
 
   return (
     <S.Container id="about">
-      <S.Content>
-       <Animation animateIn="animate__bounceInRight" animateOnce={true}>
+    <S.Content>
+      <Animation animateIn="animate__bounceInRight" animateOnce={true}>
         <S.Header> About </S.Header>
-       </Animation>
-       <Animation animateIn="animate__flipInX" animateOnce={true} delay={200}>
-        <S.Para>
-          <span role='img' aria-label='sayHello'> 👋 </span>{' '}
-          Hey, I am a Full Stack Web Developer and Programmer from Punjab, India.
-        </S.Para>
-       </Animation>
-       <Animation animateIn="animate__flipInX" animateOnce={true} delay={200}>
-        <S.Para>
-          I am an undegraduate
-          <span role='img' aria-label='alma-matter'> 👨‍🎓 </span>{' '}
-          from
-          <S.Link> National Institute of Technology, Jalandhar </S.Link>
-          pursuing majors in Instrumentation and Control Engineering
-          and minor in Computer Science Engineering. <br />
-          I love to write clean and efficient code <span role='img' aria-label='coding'> 👨🏼‍💻 </span>. <br />
-          I have a great interest in learning new technologies and implementing them in real life.
-        </S.Para>
-       </Animation>
-       <Animation animateIn="animate__flipInX" animateOnce={true} delay={200}>
-        <S.Para>
-          Apart from development I like to program microcontrollers
-          <span role='img' aria-label='robo'> 🤖 </span>{' '}
-          and also do competitive programming
-          <span role='img' aria-label='idea'> 💡 </span>{' '}
-        </S.Para>
       </Animation>
-      </S.Content>
+      <S.InnerContainer>
+      <S.Left>
+         <Animation animateIn="animate__flipInX" animateOnce={true} delay={200}>
+          <S.Para>
+            <span role='img' aria-label='sayHello'> 👋 </span>{' '}
+            Hey, I am a Full Stack Web Developer and Programmer from Punjab, India.
+          </S.Para>
+         </Animation>
+         <Animation animateIn="animate__flipInX" animateOnce={true} delay={200}>
+          <S.Para>
+            I am an undegraduate
+            <span role='img' aria-label='alma-matter'> 👨‍🎓 </span>{' '}
+            from
+            <S.Link> National Institute of Technology, Jalandhar </S.Link>
+            pursuing majors in Instrumentation and Control Engineering
+            and minor in Computer Science Engineering. <br />
+            I love to write clean and efficient code <span role='img' aria-label='coding'> 👨🏼‍💻 </span>. <br />
+            I have a great interest in learning new technologies and implementing them in real life.
+          </S.Para>
+         </Animation>
+         <Animation animateIn="animate__flipInX" animateOnce={true} delay={200}>
+          <S.Para>
+            Apart from development I like to program microcontrollers
+            <span role='img' aria-label='robo'> 🤖 </span>{' '}
+            and also do competitive programming
+            <span role='img' aria-label='idea'> 💡 </span>{' '}
+          </S.Para>
+        </Animation>
+      </S.Left>
       <S.ImageContainer className = {"hideMedium"}>
        <img src={balloon} alt="Parallax Layer"></img>
       </S.ImageContainer>
+      </S.InnerContainer>
+      </S.Content>
     </S.Container>
   )
 }
-
 export default About
--- components/About/About.style.js
+++ components/About/About.style.js
@@ -2,20 +2,27 @@ import { styled } from './../../styles/theme.config.js'
 
 export const Container = styled('div', {
   display        : 'flex',
-  justifyContent : 'center',
-  alignItems     : 'center',
+  flexDirection : 'column',
   zindex         : -1,
-  margin         : '5em 5em',
+  padding : '7em 3em',
+  background : '$primary_light',
   '@media only screen and (max-width : 600px)' : {
-    margin : '2em 2em',
+    justifyContent : 'center',
+    alignItems     : 'center',
   }
 })
 
 export const Content = styled('div', {
   display          : 'flex',
-  flexDirection    : 'column',
-  maxWidth         : '100vw'  ,
+  flexDirection : 'column',
+  '@media only screen and (max-width : 600px)' : {
+    justifyContent : 'center',
+    alignItems     : 'center',
+  }
+})
 
+export const InnerContainer = styled('div', {
+  display          : 'flex',
   '@media only screen and (max-width : 600px)' : {
     justifyContent : 'center',
     alignItems     : 'center',
@@ -28,11 +35,16 @@ export const Header = styled('h1', {
   margin   : '1em 0'
 })
 
+export const Left = styled('div' , {
+  display : 'flex',
+  flexDirection  : 'column',
+})
+
 export const Para = styled('p', {
   color      : '$white',
   fontSize   : '$2',
   fontFamily : '$secondary',
-  margin     : '15px 0'
+  margin     : '1em 0',
 })
 
 export const ImageContainer = styled('div',{
@@ -44,7 +56,6 @@ export const ImageContainer = styled('div',{
     },
     '@media only screen and (max-width : 1000px)' : {
       img : {
-
       }
     }
 })
