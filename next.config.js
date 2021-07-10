3479e2bb565082b7ac9b20eeefaf70d08bdd3f97
hardikchopra242 <hardikchopra242@gmail.com>
2021-07-10 23:34:50 +0530

    [Mod] added support for png + svg

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
 const withImages = require('next-images')
-module.exports = withImages()
+module.exports = withImages({
+  future: {
+    webpack5: true
+  },
+  fileExtensions: ["png","svg"],
+  webpack(config, options) {
+    return config
+  }
+})
